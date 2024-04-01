5839f7a74b090a6ba46292cc5705dee405abd5ff
Kessya Monique <kessyaquerino6@gmail.com>
2024-04-01 00:04:37 -0300

    segundo commit do JS

--- script.js
+++ script.js
@@ -24,3 +24,12 @@ function receberQuantidadePessoas(evento) {
         pessoas = Number(evento.target.value)
     }
 }
+
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click", receberPorcentagem)
+})
+
+function receberPorcentagem(evento) {
+    console.log(evento.target.value)
+}
